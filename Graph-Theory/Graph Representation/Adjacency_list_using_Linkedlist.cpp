9e24bd36bef53bfa8ccc893196db8226a0be475e
Abd El~Twab M. Fakhry <55063723+AbdeltwabMF@users.noreply.github.com>
2020-08-20 19:05:24 +0200

    Add files via upload

--- Graph-Theory/Graph Representation/Adjacency_list_using_Linkedlist.cpp
+++ Graph-Theory/Graph Representation/Adjacency_list_using_Linkedlist.cpp
@@ -1,7 +1,3 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 const int N = 1e5 + 5, M = 1e6 + 6; /** N -> Number of nodes, M -> Number of edges **/
 
 int Head[N], Next[M], To[M], From[M], ne; /** ne -> an edge index **/
@@ -20,20 +16,4 @@ void dfs(int node)
 	visited[node] = true;
 	for(int i = Head[node]; i; i = Next[i]) if(!visited[To[i]])
 		dfs(To[i]);
-}
-
-int main()
-{
-	int n, m;
-	cin >> n >> m;
-	
-	int from, to;
-	while(m--)
-	{
-		cin >> from >> to;
-		addEdge(from, to); addEdge(to, from);
-	}
-
-	for(int i = 1; i <= n; ++i) if(!visited[i]) 
-		dfs(i);
 }
