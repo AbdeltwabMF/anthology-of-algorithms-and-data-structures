3381187f577fdf90cfccb738c5948ed31f713d0f
Abd El~Twab M. Fakhry <55063723+AbdeltwabMF@users.noreply.github.com>
2020-08-20 19:01:03 +0200

    Add files via upload

--- Mathematics/Number Theory/Prime Generation/Optimized_Sieve_Eratosthenes.cpp
+++ Mathematics/Number Theory/Prime Generation/Optimized_Sieve_Eratosthenes.cpp
@@ -1,22 +1,30 @@
-vector <int> sieve(int n)
+const int N = 1e9 + 9; /** MAX N **/
+
+bitset <N> isPrime;
+
+void Precomputation_Sieve()
 {
-    bitset <1000000009> isPrime; isPrime.set();
-    vector <int> Primes;
-
-    if(n >= 2) Primes.push_back(2);
-    if(n >= 3) Primes.push_back(3);
-
-    int _sqrt = sqrtl(n);
-    for(int i = 5; i <= _sqrt; i += 6)
+    isPrime.set();
+    int _sqrt = sqrtl(N);
+    
+	for(int i = 5; i <= _sqrt; i += 6)
     {
-        if(isPrime[i]) for (int j = i * i; j <= n; j += i + i) isPrime.reset(j); i += 2;
-        if(isPrime[i]) for (int j = i * i; j <= n; j += i + i) isPrime.reset(j); i -= 2;
+        if(isPrime[i]) for (int j = i * i; j < N; j += i + i) isPrime.reset(j); i += 2;
+        if(isPrime[i]) for (int j = i * i; j < N; j += i + i) isPrime.reset(j); i -= 2;
     }
+}
 
-    for (int i = 5; i <= n; i += 6)
+vector <int> Primes(int n)  /** Primes up to n **/
+{
+	vector <int> _Primes;
+	
+	if(n >= 2) _Primes.push_back(2);
+    if(n >= 3) _Primes.push_back(3);
+	
+	for (int i = 5; i <= n; i += 6)
     {
-        if(isPrime[i]) Primes.push_back(i); i += 2;
-        if(isPrime[i]) Primes.push_back(i); i -= 2;
+        if(isPrime[i]) _Primes.push_back(i); i += 2;
+        if(isPrime[i]) _Primes.push_back(i); i -= 2;
     }
-    return Primes;
+    return _Primes;
 }
