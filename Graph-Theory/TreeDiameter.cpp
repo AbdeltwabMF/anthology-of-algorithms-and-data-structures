dc9caf77279b51f36bdffd100817212457558da3
Abd El~Twab M. Fakhry <55063723+AbdeltwabMF@users.noreply.github.com>
2020-08-22 05:20:36 +0200

    Update TreeDiameter.cpp

--- Graph-Theory/TreeDiameter.cpp
+++ Graph-Theory/TreeDiameter.cpp
@@ -8,31 +8,30 @@ int Head[N], Next[N << 1], To[N << 1], ne, at, from, to, diameter, u, v, n;
 
 void addEdge(int _from, int _to)
 {
-    Next[++ne] = Head[_from];
-    Head[_from] = ne;
-    To[ne] = _to;
+    	Next[++ne] = Head[_from];
+    	Head[_from] = ne;
+    	To[ne] = _to;
 }
 
 void DFS(int _node, int parent, int cost)
 {
-    if(cost > diameter) diameter = cost, at = _node; 
+    	if(cost > diameter) diameter = cost, at = _node; 
 
-    for (int e = Head[_node]; e; e = Next[e]) if(To[e] != parent)
-        DFS(To[e], _node, cost + 1);              
+    	for (int e = Head[_node]; e; e = Next[e]) if(To[e] != parent)
+        	DFS(To[e], _node, cost + 1);              
 }
 
 int main()
 {
-    cin >> n;
+	cin >> n;
+	while(--n)
+	{
+       		cin >> u >> v;
+       		addEdge(u, v); addEdge(v, u);
+	}
 
-    while(--n)
-    {
-        cin >> u >> v;
-        addEdge(u, v); addEdge(v, u);
-    }
-
-    DFS(1, -1, 0);    from = at, diameter = 0; 
+    	DFS(1, -1, 0);    from = at, diameter = 0; 
 	DFS(from, -1, 0);   to = at;          
 	
-    cout << diameter << " " << from << " " << to << endl;
+    	cout << diameter << " " << from << " " << to << endl;
 }
