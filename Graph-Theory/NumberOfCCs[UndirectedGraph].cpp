c9948a4f46f13b63fea10987f14692188037516b
Abd El~Twab M. Fakhry <55063723+AbdeltwabMF@users.noreply.github.com>
2020-08-22 05:23:00 +0200

    Update NumberOfCCs[UndirectedGraph].cpp

--- Graph-Theory/NumberOfCCs[UndirectedGraph].cpp
+++ Graph-Theory/NumberOfCCs[UndirectedGraph].cpp
@@ -9,33 +9,31 @@ bool visited[N];
 
 void addEdge(int _from, int _to)
 {
-    Next[++ne] = Head[_from];
-    Head[_from] = ne;
-    To[ne] = _to;
+        Next[++ne] = Head[_from];
+        Head[_from] = ne;
+        To[ne] = _to;
 }
 
 void DFS(int _node)
 {
-    visited[_node] = true;
-    for (int e = Head[_node]; e; e = Next[e]) if(!visited[To[e]])
-        DFS(To[e]);
+        visited[_node] = true;
+        for (int e = Head[_node]; e; e = Next[e]) if(!visited[To[e]])
+                DFS(To[e]);
 }
 
 int main()
 {
-    cin >> n >> m;
+        cin >> n >> m;
+        while(m--)
+        {
+                cin >> u >> v;
+                addEdge(u, v); addEdge(v, u);
+        }
 
-    while(m--)
-    {
-        cin >> u >> v;
-        addEdge(u, v); addEdge(v, u);
-    }
-
-    for (int node = 1; node <= n; ++node) if(!visited[node])
-    {
-        ++CCs;
-        DFS(node);
-    }
-
-    cout << CCs << endl;
+        for (int node = 1; node <= n; ++node) if(!visited[node])
+        {
+                ++CCs;
+                DFS(node);
+        }
+        cout << CCs << endl;
 }
