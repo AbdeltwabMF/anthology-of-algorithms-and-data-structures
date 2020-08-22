0cec968ae7dddd913072d0199ee78198fb141667
Abd El~Twab M. Fakhry <55063723+AbdeltwabMF@users.noreply.github.com>
2020-08-22 05:24:19 +0200

    Update NumberOfCCs[ForestTree].cpp

--- Graph-Theory/NumberOfCCs[ForestTree].cpp
+++ Graph-Theory/NumberOfCCs[ForestTree].cpp
@@ -9,33 +9,31 @@ bool visited[N];
 
 void addEdge(int _from, int _to)
 {
-    Next[++ne] = Head[_from];
-    Head[_from] = ne;
-    To[ne] = _to;
+        Next[++ne] = Head[_from];
+        Head[_from] = ne;
+        To[ne] = _to;
 }
 
 void DFS(int _node, int parent)
 {
-    visited[_node] = true;
-    for (int e = Head[_node]; e; e = Next[e]) if(To[e] != parent)
-        DFS(To[e], _node);
+        visited[_node] = true;
+        for (int e = Head[_node]; e; e = Next[e]) if(To[e] != parent)
+                DFS(To[e], _node);
 }
 
 int main()
 {
-    cin >> n;
+        cin >> n;
+        while(--n)
+        {
+                cin >> u >> v;
+                addEdge(u, v); addEdge(v, u);
+        }
 
-    while(--n)
-    {
-        cin >> u >> v;
-        addEdge(u, v); addEdge(v, u);
-    }
-
-    for (int node = 1; node <= n; ++node) if(!visited[node])
-    {
-        ++CCs;
-        DFS(node, -1);
-    }
-
-    cout << CCs << endl;
+        for (int node = 1; node <= n; ++node) if(!visited[node])
+        {
+                ++CCs;
+                DFS(node, -1);
+        }
+        cout << CCs << endl;
 }
