88c8b162c2488da932f87d9950a0f84a207010ac
Abd El~Twab M. Fakhry <55063723+AbdeltwabMF@users.noreply.github.com>
2020-08-22 05:25:37 +0200

    Update CycleDetection[UndirectedGraph_dfs].cpp

--- Graph-Theory/CycleDetection[UndirectedGraph_dfs].cpp
+++ Graph-Theory/CycleDetection[UndirectedGraph_dfs].cpp
@@ -9,32 +9,31 @@ bool visited[N], hasCycle;
 
 void addEdge(int _from, int _to)
 {
-    Next[++ne] = Head[_from];
-    Head[_from] = ne;
-    To[ne] = _to;
+    	Next[++ne] = Head[_from];
+    	Head[_from] = ne;
+    	To[ne] = _to;
 }
 
 void DFS(int _node, int parent)
 {
 	if(hasCycle |= visited[_node]) return;
-    visited[_node] = true;
+    	visited[_node] = true;
 	
-    for (int e = Head[_node]; e; e = Next[e]) if(To[e] != parent)
-        DFS(To[e], _node);
+    	for (int e = Head[_node]; e; e = Next[e]) if(To[e] != parent)
+        	DFS(To[e], _node);
 }
 
 int main()
 {
-    cin >> n >> m;
-
-    while(m--)
-    {
-        cin >> u >> v;
+    	cin >> n >> m;
+    	while(m--)
+    	{
+        	cin >> u >> v;
 		addEdge(u, v); addEdge(v, u);
-    }
+    	}
 
-    for (int node = 1; node <= n; ++node) if(!visited[node])
-        DFS(node, -1); 
+    	for (int node = 1; node <= n; ++node) if(!visited[node])
+        	DFS(node, -1); 
 	
 	cout << (hasCycle ? "YES" : "NO") << endl;
 }
