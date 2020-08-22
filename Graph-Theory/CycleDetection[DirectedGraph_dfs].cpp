da7e3561e56cab36d1ea74009238569dc1a2d442
Abd El~Twab M. Fakhry <55063723+AbdeltwabMF@users.noreply.github.com>
2020-08-22 05:27:29 +0200

    Update CycleDetection[DirectedGraph_dfs].cpp

--- Graph-Theory/CycleDetection[DirectedGraph_dfs].cpp
+++ Graph-Theory/CycleDetection[DirectedGraph_dfs].cpp
@@ -10,38 +10,33 @@ char visited[N];
 
 void addEdge(int _from, int _to)
 {
-    Next[++ne] = Head[_from];
-    Head[_from] = ne;
-    To[ne] = _to;
+    	Next[++ne] = Head[_from];
+   	Head[_from] = ne;
+    	To[ne] = _to;
 }
 
 void DFS(int _node)
 {
-	/** Oops, revisiting active node **/
-	if(hasCycle |= visited[_node] == 1) return; 
-    
-	/** current node legend mode has been activated **/
-	visited[_node] = 1; 
+	if(hasCycle |= visited[_node] == 1) return; /** Oops, revisiting active node **/
+	visited[_node] = 1;                         /** current node legend mode has been activated **/
 	
-    for (int e = Head[_node]; e; e = Next[e]) if(visited[To[e]] != 2)
-        DFS(To[e]);
-		
-	/** done with this node and mark it as visited **/
-	visited[node] = 2; 
+    	for (int e = Head[_node]; e; e = Next[e]) if(visited[To[e]] != 2)
+        	DFS(To[e]);
+	
+	visited[node] = 2;                          /** done with this node and mark it as visited **/
 }
 
 int main()
 {
-    cin >> n >> m;
+    	cin >> n >> m;
+    	while(m--)
+    	{
+        	cin >> u >> v;
+        	addEdge(u, v);
+    	}
 
-    while(m--)
-    {
-        cin >> u >> v;
-        addEdge(u, v);
-    }
-
-    for (int node = 1; node <= n; ++node) if(!visited[node])
-        DFS(node);
+    	for (int node = 1; node <= n; ++node) if(!visited[node])
+        	DFS(node);
 	
 	cout << (hasCycle ? "YES" : "NO") << endl;
 }
