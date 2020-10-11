7ef35f9a4372d8ef0ffb3aa0d36b0292d94b2d73
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-10-11 05:11:45 +0200

    Add Files

---
+++ Miscellaneous/Indexed-Associative-Containers.cpp
@@ -0,0 +1,18 @@
+#if      __cplusplus >= 201402L
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+#endif
+
+#if   __cplusplus >= 201402L    \
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+#endif
+
+\1
+template <class T, typename Comp = less <T> >
+using indexed_set = tree <T, null_type, Comp, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename K, typename V, typename Comp = less <K>>
+using indexed_map = tree <K, V, Comp, rb_tree_tag, tree_order_statistics_node_update>;
+
