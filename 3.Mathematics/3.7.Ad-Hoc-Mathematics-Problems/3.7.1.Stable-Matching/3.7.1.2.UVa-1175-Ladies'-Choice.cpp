22cfd65024bcd634e71f4ac5d50d94877cad3b0d
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-10-21 08:19:19 +0200

    Update Stable Marriage

--- 3.Mathematics/3.7.Ad-Hoc-Mathematics-Problems/3.7.1.Stable-Matching/3.7.1.2.UVa-1175-Ladies'-Choice.cpp
+++ 3.Mathematics/3.7.Ad-Hoc-Mathematics-Problems/3.7.1.Stable-Matching/3.7.1.2.UVa-1175-Ladies'-Choice.cpp
@@ -1,53 +1,63 @@
+/** https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3616
+**/
+
+#pragma GCC optimize ("Ofast")
+
 #include <bits/stdc++.h>
 
-#define  endl   '\n'
+#define endl      '\n'
 
 using namespace std;
 
+typedef int64_t    ll;
+typedef __int128 i128;
+
 void Fast() {
     cin.sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(0);cout.tie(0);
 }
 
-const int N = 1e3 + 9;
-
-/**----------------->>  Quality Over Quantity  <<----------------**/
-
-int pref[N][N], order[N][N], Next[N], wife[N], husband[N], n, u;
+const int N = 1e3+ 9, M = 1e3 + 9, oo = 0x3f3f3f3f;
 queue <int> Q;
 
-void engage(int man, int woman)
-{
-    int ex = wife[man];
-    husband[woman] = man;
-    wife[man] = woman;
-    if(ex) Q.push(ex);
-}
+int husband[N], wife[N], Next[N], order[N][N], pref[N][N], n, v;
 
 void _clear() {
-    memset(husband, 0, (n + 2) << 2);
-    memset(wife, 0, (n + 4) << 2);
-    memset(Next, 0, (n + 4) << 2);
-    Q = queue <int> ();
+    memset(wife,    0, sizeof(wife[0])    * (n + 2));
+    memset(husband, 0, sizeof(husband[0]) * (n + 2));
+    memset(Next,    0, sizeof(Next[0])    * (n + 2));
 }
 
-void Solve() {
-    /** Ladies' Choice, the best possible choice for the girls. **/
-    cin >> n;
+void engage(int man, int woman) {
+    int exWife = wife[man];
+    wife[man] = woman;
+    husband[woman] = man;
+
+    if(exWife)
+        Q.push(exWife);
+}
+
+void Solve()
+{
     _clear();
 
-    for(int i = 1; i <= n; ++i) {
-        for(int j = 1; j <= n; ++j) cin >> pref[i][j];
-        Q.push(i);
-    }
-
+    cin >> n;
     for(int i = 1; i <= n; ++i)
         for(int j = 1; j <= n; ++j)
-            cin >> u, order[i][u] = j;
+            cin >> pref[i][j];
+
+    for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= n; ++j) {
+            cin >> v;
+            order[i][v] = j;
+        }
+
+    for(int i = 1; i <= n; ++i)
+        Q.push(i);
 
     int man, woman;
-    while(Q.size()) {
+    while(Q.size())
+    {
         woman = Q.front(); Q.pop();
         man = pref[woman][++Next[woman]];
 
@@ -58,22 +68,32 @@ void Solve() {
     }
 
     for(int i = 1; i <= n; ++i)
-        cout << husband[i] << endl; /** the i-th woman's husband */
+        cout << husband[i] << endl;
 }
 
-void MultiTest(bool Tests = 0)
+int main()
 {
-    int tc = 1; (Tests) && (cin >> tc);
+    Fast();
+
+    int tc = 1; cin >> tc;
     for(int i = 1; i <= tc; ++i) {
         if(i > 1) cout << endl;
         Solve();
     }
 }
 
-/**------------------------->>  Main  <<-------------------------**/
-
-int main()
-{
-    Fast(); MultiTest(1);
-}
+/**
+1
+3
+3 2 1
+2 1 3
+2 1 3
+3 1 2
+1 2 3
+2 3 1
+-----
+3
+2
+1
+**/
 
