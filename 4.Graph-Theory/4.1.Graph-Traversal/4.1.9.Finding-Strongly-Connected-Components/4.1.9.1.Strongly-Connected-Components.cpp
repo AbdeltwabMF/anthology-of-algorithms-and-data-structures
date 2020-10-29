7b3b5f1789ef8fc848f3ca14c842caf0a3b4347f
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-10-29 11:29:47 +0200

    Add SCCs

--- 4.Graph-Theory/4.1.Graph-Traversal/4.1.9.Finding-Strongly-Connected-Components/4.1.9.1.Strongly-Connected-Components.cpp
+++ 4.Graph-Theory/4.1.Graph-Traversal/4.1.9.Finding-Strongly-Connected-Components/4.1.9.1.Strongly-Connected-Components.cpp
@@ -21,7 +21,7 @@ void File() {
 const int N = 1e3 + 9, M = 2e5 + 9, oo = 0x3f3f3f3f;
 ll INF = 0x3f3f3f3f3f3f3f3f;
 
-int Head[N], Next[M], To[M], Par[N], dfs_num[N], dfs_low[N], ne, n, m, u, v, dfs_timer;
+int Head[N], Next[M], To[M], dfs_num[N], dfs_low[N], ne, n, m, u, v, dfs_timer;
 stack <int> stk;
 bool in_stk[N];
 vector< vector <int> > SCCs;
@@ -33,7 +33,6 @@ void addEdge(int from, int to) {
 }
 
 void _clear() {
-    memset(Par,         -1, sizeof(Par[0])          * (n + 2));
     memset(Head,         0, sizeof(Head[0])         * (n + 2));
     memset(dfs_num,      0, sizeof(dfs_num[0])      * (n + 2));
     memset(in_stk,       0, sizeof(in_stk[0])       * (n + 2));
@@ -51,13 +50,12 @@ void DFS(int node)
     for(int i = Head[node]; i; i = Next[i])
     {
         if(dfs_num[To[i]] == 0) {
-            Par[To[i]] = node;
             DFS(To[i]);
 
             if(dfs_low[To[i]] < dfs_low[node])
                 dfs_low[node] = dfs_low[To[i]];
         }
-        else if(Par[node] != To[i])
+        else if(in_stk[To[i]])
             if(dfs_num[To[i]] < dfs_low[node])
                 dfs_low[node] = dfs_num[To[i]];
     }
@@ -98,7 +96,7 @@ void Solve()
         cout << "Component : ";
 
         for(int i = 0; i < CC.size(); ++i)
-            cout << i << " \n"[i == CC.size() - 1];
+            cout << CC[i] << " \n"[i == CC.size() - 1];
     }
 }
 
