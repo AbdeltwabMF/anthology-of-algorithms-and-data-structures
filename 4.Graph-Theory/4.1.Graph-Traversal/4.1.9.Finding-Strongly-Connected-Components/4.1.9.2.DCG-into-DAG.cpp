d040ed054574eeb1857c202aeedfbb06874e184e
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-10-30 15:52:32 +0200

    Add DCG into  DAG

---
+++ 4.Graph-Theory/4.1.Graph-Traversal/4.1.9.Finding-Strongly-Connected-Components/4.1.9.2.DCG-into-DAG.cpp
@@ -0,0 +1,120 @@
+#pragma GCC optimize ("Ofast")
+
+#include <bits/stdc++.h>
+
+#define endl        '\n'
+#define Min(a, b)   (((a) < (b)) ? (a) : (b));
+
+using namespace std;
+
+typedef int64_t  ll;
+
+void Fast() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+}
+
+void File() {
+    freopen("input.in",  "r", stdin);
+    freopen("output.out", "w", stdout);
+}
+
+const int N = 1e5 + 9, M = 2e6 + 9, oo = 0x3f3f3f3f;
+ll INF = 0x3f3f3f3f3f3f3f3f;
+
+int Head[N], To[M], Next[M], dfs_num[N], dfs_low[N], n, m, u, v, tax;
+int dfs_timer, top, ne, ID;
+int Stack[N], out[N], compID[N], compSize[N];
+bool in_stack[N];
+
+int HeadDAG[N], ToDAG[M], NextDAG[M], neDAG;
+
+void addEdge(int from, int to) {
+    Next[++ne] = Head[from];
+    Head[from] = ne;
+    To[ne] = to;
+}
+
+void addEdgeDAG(int from, int to) {
+    NextDAG[++neDAG] = HeadDAG[from];
+    HeadDAG[from] = neDAG;
+    ToDAG[neDAG] = to;
+    ++out[from];
+}
+
+void _clear() {
+    memset(out,      0, sizeof(out[0])      * (n + 2));
+    memset(Head,     0, sizeof(Head[0])     * (n + 2));
+    memset(compID,   0, sizeof(compID[0])   * (n + 2));
+    memset(HeadDAG,  0, sizeof(HeadDAG[0])  * (n + 2));
+    memset(dfs_num,  0, sizeof(dfs_num[0])  * (n + 2));
+    memset(compSize, 0, sizeof(compSize[0]) * (n + 2));
+    ne = dfs_timer = top = neDAG = ID = 0;
+}
+
+void Tarjan(int node)
+{
+    dfs_num[node] = dfs_low[node] = ++dfs_timer;
+    in_stack[Stack[top++] = node] = true;
+
+    for(int i = Head[node]; i; i = Next[i])
+    {
+        if(dfs_num[To[i]] == 0)
+            Tarjan(To[i]);
+
+        if(in_stack[To[i]])
+            dfs_low[node] = Min(dfs_low[node], dfs_low[To[i]]);
+    }
+
+    if(dfs_num[node] == dfs_low[node])
+    {
+        ++ID;
+        for(int cur = -1; cur ^ node;)
+        {
+            in_stack[cur = Stack[--top]] = false;
+            compID[cur] = ID;
+            ++compSize[ID];
+        }
+    }
+}
+
+void DFS(int node)
+{
+    dfs_num[node] = 1;
+    for(int i = Head[node]; i; i = Next[i])
+    {
+        if(compID[node] != compID[To[i]])
+            addEdgeDAG(compID[node], compID[To[i]]);
+
+        if(dfs_num[To[i]] == 0)
+            DFS(To[i]);
+    }
+}
+
+void Solve()
+{
+    cin >> n >> m;
+    _clear();
+
+    while(m--) {
+        cin >> u >> v;
+        addEdge(u, v);
+    }
+
+    for(int i = 1; i <= n; ++i) if(dfs_num[i] == 0)
+            Tarjan(i);
+
+    memset(dfs_num, 0, sizeof(dfs_num[0]) * (n + 2));
+    for(int i = 1; i <= n; ++i) if(dfs_num[i] == 0)
+            DFS(i);
+}
+
+int main()
+{
+    Fast();
+
+    int tc = 1;
+    for(int i = 1; i <= tc; ++i)
+        Solve();
+}
+
