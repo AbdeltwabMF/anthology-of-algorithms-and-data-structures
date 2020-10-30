2abf441cd0a0871c87c5116da8dd9366f7cd162c
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-10-30 15:52:15 +0200

    Update Art Points and Bridges

--- 4.Graph-Theory/4.1.Graph-Traversal/4.1.8.Finding-Articulation-Points-and-Bridges/4.1.8.1.Articulation-Points-and-Bridges.cpp
+++ 4.Graph-Theory/4.1.Graph-Traversal/4.1.8.Finding-Articulation-Points-and-Bridges/4.1.8.1.Articulation-Points-and-Bridges.cpp
@@ -2,6 +2,9 @@
 
 #include <bits/stdc++.h>
 
+#define endl        '\n'
+#define Min(a, b)   (((a) < (b)) ? (a) : (b));
+
 using namespace std;
 
 typedef int64_t  ll;
@@ -11,13 +14,18 @@ void Fast() {
     cin.tie(0);cout.tie(0);
 }
 
+void File() {
+    freopen("input.in",  "r", stdin);
+    freopen("output.out", "w", stdout);
+}
+
 const int N = 1e5 + 9, M = 2e5 + 9, oo = 0x3f3f3f3f;
 ll INF = 0x3f3f3f3f3f3f3f3f;
 
-int Head[N], Next[M], To[M], Par[N], dfs_num[N], dfs_low[N], ne, n, m, u, v, dfs_timer;
-vector < pair <int, int> > bridges;
+int Head[N], Next[M], To[M], Par[N], dfs_num[N], dfs_low[N], ne, n, m, u, v, tax, dfs_timer;
+bool Art[N];
 int root, rootChildren;
-bool articulation[N];
+vector < pair <int, int> > bridges;
 
 void addEdge(int from, int to) {
     Next[++ne] = Head[from];
@@ -26,39 +34,36 @@ void addEdge(int from, int to) {
 }
 
 void _clear() {
-    memset(Par,         -1, sizeof(Par[0])          * (n + 2));
-    memset(Head,         0, sizeof(Head[0])         * (n + 2));
-    memset(dfs_num,      0, sizeof(dfs_num[0])      * (n + 2));
-    memset(articulation, 0, sizeof(articulation[0]) * (n + 2));
+    memset(Par,    -1, sizeof(Par[0])     * (n + 2));
+    memset(Art,     0, sizeof(Art[0])     * (n + 2));
+    memset(Head,    0, sizeof(Head[0])    * (n + 2));
+    memset(dfs_num, 0, sizeof(dfs_num[0]) * (n + 2));
+    ne = dfs_timer = 0;
     bridges.clear();
-    dfs_timer = 0;
-    ne = 0;
 }
 
-void DFS(int node)
+void Tarjan(int node)
 {
     dfs_num[node] = dfs_low[node] = ++dfs_timer;
 
     for(int i = Head[node]; i; i = Next[i])
     {
-        if(dfs_num[To[i]] == 0) {
-            if(node == root) rootChildren++;
+        if(dfs_num[To[i]] == 0)
+        {
+            if(node == root) ++rootChildren;
 
             Par[To[i]] = node;
-            DFS(To[i]);
-
-            if(dfs_low[To[i]] < dfs_low[node])
-                dfs_low[node] = dfs_low[To[i]];
-
-            if(dfs_low[To[i]] > dfs_num[node])
-                bridges.push_back({node, To[i]});
+            Tarjan(To[i]);
+            dfs_low[node] = Min(dfs_low[node], dfs_low[To[i]]);
 
             if(dfs_low[To[i]] >= dfs_num[node])
-                articulation[node] = true;
+                Art[node] = true;
+
+            if(dfs_low[To[i]] > dfs_num[node])
+                bridges.push_back(make_pair(node, To[i]));
         }
-        else if(Par[node] != To[i])
-            if(dfs_num[To[i]] < dfs_low[node])
-                dfs_low[node] = dfs_num[To[i]];
+        else if(To[i] != Par[node])
+            dfs_low[node] = Min(dfs_low[node], dfs_num[To[i]]);
     }
 }
 
@@ -73,22 +78,20 @@ void Solve()
         addEdge(v, u);
     }
 
-    for(int i = 1; i <= n; ++i) if(!dfs_num[i])
-    {
+    for(int i = 1; i <= n; ++i) if(dfs_num[i] == 0) {
         root = i;
         rootChildren = 0;
-
-        DFS(i);
-        articulation[root] = (rootChildren > 1);
+        Tarjan(i);
+        Art[root] = (rootChildren > 1);
     }
 
-    cout << "Cut Points :\n";
-    for(int i = 1; i <= n; ++i) if(articulation[i])
+    cout << "Art Points :\n";
+    for(int i = 1; i <= n; ++i) if(Art[i])
             cout << i << " ";
 
     cout << "\nBridges :\n";
     for(pair <int, int> bridge : bridges)
-        cout << bridge.first << " <--> " << bridge.second << endl;
+        cout << bridge.first << " - " << bridge.second << endl;
 }
 
 int main()
@@ -96,6 +99,7 @@ int main()
     Fast();
 
     int tc = 1;
-    while(tc--) Solve();
+    for(int i = 1; i <= tc; ++i)
+        Solve();
 }
 
