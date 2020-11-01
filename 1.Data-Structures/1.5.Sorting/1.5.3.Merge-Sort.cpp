47d41231ebb06e1d2032390b1c948ce062edaab8
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-11-01 19:21:12 +0200

    Add Merge Sort

--- 1.Data-Structures/1.5.Sorting/1.5.3.Merge-Sort.cpp
+++ 1.Data-Structures/1.5.Sorting/1.5.3.Merge-Sort.cpp
@@ -1,24 +1,47 @@
-void merge(int localArr [], int l, int mid, int r) {
+/**
+	Time Complexity: Sorting arrays on different machines. Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation. 
+	T(n) = 2T(n/2) + θ(n)
+
+	The above recurrence can be solved either using the Recurrence Tree method or the Master method. It falls in case II of Master Method and the solution of the recurrence is θ(nLogn). Time complexity of Merge Sort is  θ(nLogn) in all 3 cases (worst, average and best) as merge sort always divides the array into two halves and takes linear time to merge two halves.
+	Auxiliary Space: O(n)
+	Algorithmic Paradigm: Divide and Conquer
+	Sorting In Place: No in a typical implementation
+	Count Inversion of array: yes
+	Stable: Yes
+**/
+
+ll inversions;
+
+template <class T>
+void merge(T localArr [], int l, int mid, int r)
+{
     int l_size = mid - l + 1;
     int r_size = r - mid;
 
-    int L[l_size + 1];
-    int R[r_size + 1];
+    T L[l_size + 1];
+    T R[r_size + 1];
 
     for(int i = 0; i < l_size; ++i) L[i] = localArr[i + l];
     for(int i = 0; i < r_size; ++i) R[i] = localArr[i + mid + 1];
 
-    L[l_size] = R[r_size] = INT_MAX;
+    T Mx;
+    if(sizeof(T) == 4) Mx = INT_MAX;
+    else Mx = LONG_MAX;
+
+    L[l_size] = R[r_size] = Mx;
 
     for(int k = l, i = 0, j = 0; k <= r; ++k)
         if(L[i] <= R[j])
-            localArr[k] = L[i++];
+            localArr[k] = L[i], i += (L[i] != Mx);
         else
-            localArr[k] = R[j++];
+            localArr[k] = R[j], j += (R[j] != Mx), inversions += l_size - i;
 }
 
-void merge_sort(int localArr [], int l, int r) {
-    if(r - l) {
+template <class T>
+void merge_sort(T localArr [], int l, int r)
+{
+    if(r - l)
+    {
         int mid = (l + r) >> 1;
         merge_sort(localArr, l,     mid);
         merge_sort(localArr, mid + 1, r);
@@ -27,9 +50,10 @@ void merge_sort(int localArr [], int l, int r) {
 }
 
 template <class T>
-void merge_sort(T _begin, T _end) {
+void merge_sort(T _begin, T _end)
+{
     const int sz = _end - _begin;
-    int localArray[sz];
+    __typeof(*_begin) localArray[sz];
 
     __typeof(_begin) k = _begin;
     for(int i = 0; k != _end; ++i, ++k)
@@ -41,3 +65,4 @@ void merge_sort(T _begin, T _end) {
     for(int i = 0; k != _end; ++i, ++k)
         *k = localArray[i];
 }
+
