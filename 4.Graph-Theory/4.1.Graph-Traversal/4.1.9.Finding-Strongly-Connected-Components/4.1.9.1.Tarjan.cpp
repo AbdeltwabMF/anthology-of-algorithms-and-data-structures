9db89027fe1f95daa699c0169566f55cd2761f8d
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-11-01 11:11:26 +0200

    Add Tarjan SCCs

--- 4.Graph-Theory/4.1.Graph-Traversal/4.1.9.Finding-Strongly-Connected-Components/4.1.9.1.Tarjan.cpp
+++ 4.Graph-Theory/4.1.Graph-Traversal/4.1.9.Finding-Strongly-Connected-Components/4.1.9.1.Tarjan.cpp
@@ -3,7 +3,8 @@
 #include <bits/stdc++.h>
 
 #define endl        '\n'
-#define Min(a, b)   (((a) < (b)) ? (a) : (b));
+#define Min(a, b)   (((a) < (b)) ? (a) : (b))
+#define Max(a, b)   (((a) > (b)) ? (a) : (b))
 
 using namespace std;
 
@@ -23,40 +24,46 @@ const int N = 1e5 + 9, M = 2e6 + 9, oo = 0x3f3f3f3f;
 ll INF = 0x3f3f3f3f3f3f3f3f;
 
 int Head[N], To[M], Next[M], dfs_num[N], dfs_low[N], n, m, u, v, tax;
-int dfs_timer, component, top, ne;
-int Stack[N];
+int Stack[N], compID[N], compSize[N];
+int dfs_timer, top, ne, ID;
 bool in_stack[N];
-
+ 
 void addEdge(int from, int to) {
     Next[++ne] = Head[from];
     Head[from] = ne;
     To[ne] = to;
 }
-
+ 
 void _clear() {
-    memset(Head,    0, sizeof(Head[0])    * (n + 2));
-    memset(dfs_num, 0, sizeof(dfs_num[0]) * (n + 2));
-    ne = dfs_timer = component = top = 0;
+    memset(Head,     0, sizeof(Head[0])     * (n + 2));
+    memset(compID,   0, sizeof(compID[0])   * (n + 2));
+    memset(dfs_num,  0, sizeof(dfs_num[0])  * (n + 2));
+    memset(compSize, 0, sizeof(compSize[0]) * (n + 2));
+    ne = dfs_timer = top = ID = 0;
 }
-
+ 
 void Tarjan(int node)
 {
     dfs_num[node] = dfs_low[node] = ++dfs_timer;
     in_stack[Stack[top++] = node] = true;
-
+ 
     for(int i = Head[node]; i; i = Next[i])
     {
         if(dfs_num[To[i]] == 0)
             Tarjan(To[i]);
-
+ 
         if(in_stack[To[i]])
             dfs_low[node] = Min(dfs_low[node], dfs_low[To[i]]);
     }
-
+ 
     if(dfs_num[node] == dfs_low[node])
     {
-        ++component;
-        for(int cur = -1; cur ^ node; in_stack[cur = Stack[--top]] = false) {}
+        ++ID;
+        for(int cur = -1; cur ^ node;) {
+            in_stack[cur = Stack[--top]] = false;
+            compID[cur] = ID;
+            ++compSize[ID];
+        }
     }
 }
 
@@ -73,7 +80,7 @@ void Solve()
     for(int i = 1; i <= n; ++i) if(dfs_num[i] == 0)
         Tarjan(i);
 
-    cout << component << endl;
+    cout << ID << endl;
 }
 
 int main()
