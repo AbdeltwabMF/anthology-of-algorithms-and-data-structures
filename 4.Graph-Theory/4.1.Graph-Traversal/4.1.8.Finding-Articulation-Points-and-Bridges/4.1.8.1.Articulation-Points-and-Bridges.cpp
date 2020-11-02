e2563f8c0abddcf2d91596258723bd3142669f86
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-11-02 13:48:32 +0200

    Update Tarjan Art Points and Bridges

--- 4.Graph-Theory/4.1.Graph-Traversal/4.1.8.Finding-Articulation-Points-and-Bridges/4.1.8.1.Articulation-Points-and-Bridges.cpp
+++ 4.Graph-Theory/4.1.Graph-Traversal/4.1.8.Finding-Articulation-Points-and-Bridges/4.1.8.1.Articulation-Points-and-Bridges.cpp
@@ -19,27 +19,29 @@ void File() {
     freopen("output.out", "w", stdout);
 }
 
-const int N = 1e5 + 9, M = 2e5 + 9, oo = 0x3f3f3f3f;
+const int N = 1e5 + 9, M = 2e6 + 9, oo = 0x3f3f3f3f, Mod = 1e9 + 7;
 ll INF = 0x3f3f3f3f3f3f3f3f;
 
-int Head[N], Next[M], To[M], Par[N], dfs_num[N], dfs_low[N], ne, n, m, u, v, tax, dfs_timer;
+int Head[N], Next[M], To[M], Cost[M];
+int Par[N], dfs_num[N], dfs_low[N];
+int ne, n, m, u, v, w;
+int root, rootChildren, dfs_timer, bridgeInx;
 bool Art[N];
-int root, rootChildren;
-vector < pair <int, int> > bridges;
+vector < pair <int, int> > bridges(M);
 
-void addEdge(int from, int to) {
+void addEdge(int from, int to, int cost = 0) {
     Next[++ne] = Head[from];
     Head[from] = ne;
+    Cost[ne] = cost;
     To[ne] = to;
 }
 
 void _clear() {
-    memset(Par,    -1, sizeof(Par[0])     * (n + 2));
-    memset(Art,     0, sizeof(Art[0])     * (n + 2));
     memset(Head,    0, sizeof(Head[0])    * (n + 2));
     memset(dfs_num, 0, sizeof(dfs_num[0]) * (n + 2));
-    ne = dfs_timer = 0;
-    bridges.clear();
+    memset(Par,    -1, sizeof(Par[0])     * (n + 2));
+    memset(Art,     0, sizeof(Art[0])     * (n + 2));
+    ne = dfs_timer = bridgeInx = 0;
 }
 
 void Tarjan(int node)
@@ -60,14 +62,14 @@ void Tarjan(int node)
                 Art[node] = true;
 
             if(dfs_low[To[i]] > dfs_num[node])
-                bridges.push_back(make_pair(node, To[i]));
+                bridges[bridgeInx++] = make_pair(node, To[i]);
         }
         else if(To[i] != Par[node])
             dfs_low[node] = Min(dfs_low[node], dfs_num[To[i]]);
     }
 }
 
-void Solve()
+int main()
 {
     cin >> n >> m;
     _clear();
@@ -90,16 +92,7 @@ void Solve()
             cout << i << " ";
 
     cout << "\nBridges :\n";
-    for(pair <int, int> bridge : bridges)
-        cout << bridge.first << " - " << bridge.second << endl;
-}
-
-int main()
-{
-    Fast();
-
-    int tc = 1;
-    for(int i = 1; i <= tc; ++i)
-        Solve();
+    for(int i = 0; i < bridgeInx; ++i)
+        cout << bridges[i].first << " - " << bridges[i].second << endl;
 }
 
