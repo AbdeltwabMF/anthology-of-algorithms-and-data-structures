a5ce65a04fb90bb2c2185044221411be8290386c
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-11-02 13:48:50 +0200

    Update Kahn's Algorithm

--- 4.Graph-Theory/4.1.Graph-Traversal/4.1.5.Topological-Sort/4.1.5.2.TS[KahnsAlgorithm].cpp
+++ 4.Graph-Theory/4.1.Graph-Traversal/4.1.5.Topological-Sort/4.1.5.2.TS[KahnsAlgorithm].cpp
@@ -12,18 +12,20 @@ void addEdge(int from, int to) {
     To[ne] = to;
 }
 
-vector <int> kahn(int n) {
+vector <int> kahn(int n)
+{
     vector <int> ready, ret;
 
     for(int i = 1; i <= n; ++i) if(!in[i])
         ready.push_back(i);
 
-    int tmp;
-    while(!ready.empty()) {
-        tmp = ready.back(); ready.pop_back();
+    int node;
+    while(!ready.empty())
+    {
+        node = ready.back(); ready.pop_back();
+        ret.push_back(node);
 
-        ret.push_back(tmp);
-        for(int i = Head[tmp]; i; i = Next[i]) if(--in[To[i]] == 0)
+        for(int i = Head[node]; i; i = Next[i]) if(--in[To[i]] == 0)
             ready.push_back(To[i]);
     }
     return ret;
@@ -40,6 +42,7 @@ int main() {
     }
 
     vector <int> v = kahn(n);
+
     if((int)v.size() == n) for(int i : v)
             cout << i << ' ';
     else
