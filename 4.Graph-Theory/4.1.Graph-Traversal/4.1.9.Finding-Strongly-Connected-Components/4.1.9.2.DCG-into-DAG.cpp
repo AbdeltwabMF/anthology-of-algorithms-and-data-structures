700aeb49ba9792b3ad23cf0761a18105e0fbf0fc
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-11-02 13:47:49 +0200

    Update DCG into DAG

--- 4.Graph-Theory/4.1.Graph-Traversal/4.1.9.Finding-Strongly-Connected-Components/4.1.9.2.DCG-into-DAG.cpp
+++ 4.Graph-Theory/4.1.Graph-Traversal/4.1.9.Finding-Strongly-Connected-Components/4.1.9.2.DCG-into-DAG.cpp
@@ -1,54 +1,44 @@
-#pragma GCC optimize ("Ofast")
-
 #include <bits/stdc++.h>
-
 #define endl        '\n'
 #define Min(a, b)   (((a) < (b)) ? (a) : (b));
-
 using namespace std;
-
 typedef int64_t  ll;
+int main() {}
 
-void Fast() {
-    cin.sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-}
-
-void File() {
-    freopen("input.in",  "r", stdin);
-    freopen("output.out", "w", stdout);
-}
-
-const int N = 1e5 + 9, M = 2e6 + 9, oo = 0x3f3f3f3f;
+const int N = 1e5 + 9, M = 2e6 + 9, oo = 0x3f3f3f3f, Mod = 1e9 + 7;
 ll INF = 0x3f3f3f3f3f3f3f3f;
 
-int Head[N], To[M], Next[M], dfs_num[N], dfs_low[N], n, m, u, v, tax;
-int dfs_timer, top, ne, ID;
-int Stack[N], out[N], compID[N], compSize[N];
+int Head[N], To[M], Next[M], Cost[M];
+int dfs_num[N], dfs_low[N], out[N];
+int Stack[N], compID[N], compSize[N];
+int ne, n, m, u, v, w;
+int dfs_timer, top, ID;
 bool in_stack[N];
 
-int HeadDAG[N], ToDAG[M], NextDAG[M], neDAG;
+int HeadDAG[N], ToDAG[M], NextDAG[M], CostDAG[M], neDAG;
 
-void addEdge(int from, int to) {
+void addEdge(int from, int to, int cost = 0) {
     Next[++ne] = Head[from];
     Head[from] = ne;
+    Cost[ne] = cost;
     To[ne] = to;
 }
 
-void addEdgeDAG(int from, int to) {
+void addEdgeDAG(int from, int to, int cost = 0) {
     NextDAG[++neDAG] = HeadDAG[from];
     HeadDAG[from] = neDAG;
+    CostDAG[ne] = cost;
     ToDAG[neDAG] = to;
     ++out[from];
 }
 
 void _clear() {
-    memset(out,      0, sizeof(out[0])      * (n + 2));
     memset(Head,     0, sizeof(Head[0])     * (n + 2));
-    memset(compID,   0, sizeof(compID[0])   * (n + 2));
-    memset(HeadDAG,  0, sizeof(HeadDAG[0])  * (n + 2));
     memset(dfs_num,  0, sizeof(dfs_num[0])  * (n + 2));
+    memset(compID,   0, sizeof(compID[0])   * (n + 2));
     memset(compSize, 0, sizeof(compSize[0]) * (n + 2));
+    memset(HeadDAG,  0, sizeof(HeadDAG[0])  * (n + 2));
+    memset(out,      0, sizeof(out[0])      * (n + 2));
     ne = dfs_timer = top = neDAG = ID = 0;
 }
 
@@ -78,6 +68,11 @@ void Tarjan(int node)
     }
 }
 
+void Tarjan() {
+    for(int i = 1; i <= n; ++i) if(dfs_num[i] == 0)
+        Tarjan(i);
+}
+
 void DFS(int node)
 {
     dfs_num[node] = 1;
@@ -91,30 +86,10 @@ void DFS(int node)
     }
 }
 
-void Solve()
-{
-    cin >> n >> m;
-    _clear();
-
-    while(m--) {
-        cin >> u >> v;
-        addEdge(u, v);
-    }
-
-    for(int i = 1; i <= n; ++i) if(dfs_num[i] == 0)
-            Tarjan(i);
-
+void construct_dag() {
     memset(dfs_num, 0, sizeof(dfs_num[0]) * (n + 2));
+
     for(int i = 1; i <= n; ++i) if(dfs_num[i] == 0)
             DFS(i);
-}
-
-int main()
-{
-    Fast();
-
-    int tc = 1;
-    for(int i = 1; i <= tc; ++i)
-        Solve();
 }
 
