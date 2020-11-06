97113b47835dd00b246778b465a9dbff9b108644
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-11-06 21:01:30 +0200

    Update RSQ

--- 1.Data-Structures/1.4.Sparse-Table/1.4.3.RSMQ.cpp
+++ 1.Data-Structures/1.4.Sparse-Table/1.4.3.RSMQ.cpp
@@ -1,29 +1,34 @@
-template <class _T>
+template <class T, class F = function <T(const T &, const T &)> >
 class SparseTable
 {
     int _N;
     int _LOG;
-    vector <_T> _A;
-    vector < vector <_T> > ST;
+    vector <T> _A;
+    vector < vector <T> > ST;
     vector <int> Log;
+    F func;
 
   public :
-    template <class T>
-    SparseTable(T _begin, T _end)
-    {
-        _N = _end - _begin;
-        _LOG = log2(_N);
-        _A.assign(_N + 1, 0);
+    SparseTable() = default;
 
-        __typeof(_begin) i = _begin;
-        for(int j = 1; i != _end; i++, ++j)
-            _A[j] = *i;
+    template <class iter>
+    SparseTable(iter _begin, iter _end, F _func = [](T a, T b) { return a + b; }) : func(_func)
+    {
+        _N = distance(_begin, _end);
 
         Log.assign(_N + 1, 0);
         for(int i = 2; i <= _N; ++i)
-            Log[i] = Log[(i >> 1)] + 1;
+            Log[i] = Log[i >> 1] + 1;
 
-        ST.assign(_N + 1, vector <_T> (_LOG + 1, 0));
+        _LOG = Log[_N];
+
+        _A.assign(_N + 1, 0);
+        ST.assign(_N + 1, vector <T> (_LOG + 1, 0));
+
+        __typeof(_begin) i = _begin;
+        for(int j = 1; i != _end; ++i, ++j)
+            _A[j] = *i;
+
         build();
     }
 
@@ -37,27 +42,29 @@ class SparseTable
             k = (1 << j);
             d = (k >> 1);
 
-            for(int i = 1; i + k <= _N + 1; ++i)
-                ST[i][j] = ST[i][j - 1] + ST[i + d][j - 1];
+            for(int i = 1; i + k - 1 <= _N; ++i)
+            {
+                T const & x = ST[i][j - 1];     // starting subarray at index = i     with length = 2^{j - 1}
+                T const & y = ST[i + d][j - 1]; // starting subarray at index = i + d with length = 2^{j - 1}
+
+                ST[i][j] = func(x, y);
+            }
         }
     }
 
-    /** any interval can be uniquely represented as a union of intervals with lengths that are decreasing powers of two. 
-    	E.g. [2, 14] = [2, 9] ∪ [10, 13] ∪ [14, 14]
-    **/
-    
-    ll query(int l, int r)
+    T query(int l, int r)
     {
         int d = r - l + 1;
-        ll sum = 0;
+        T ret = 0;
+
         for(int i = l; d; i += lastBit(d), d -= lastBit(d))
-            sum += ST[i][Log[lastBit(d)]];
-        return sum;
+            ret = func(ret, ST[i][Log[lastBit(d)]]);
+
+        return ret;
     }
 
     int lastBit(int a) {
         return (a & -a);
     }
 };
-
 
