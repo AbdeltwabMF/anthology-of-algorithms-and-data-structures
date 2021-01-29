f85ad7e535e98f060169279c011d55a19cfee7e1
Abd El-Twab M. fakhry <Abdeltwab.m.fakhry@gmail.com>
2021-01-29 07:27:50 +0200

    Fix issues

--- 3.Mathematics/3.2.Number-Theory/3.2.1.Prime-Generation/3.2.1.5.Linear-Sieve.cpp
+++ 3.Mathematics/3.2.Number-Theory/3.2.1.Prime-Generation/3.2.1.5.Linear-Sieve.cpp
@@ -1,13 +1,13 @@
 int leastPrime[N];
 int Primes[664580], pnx;
 
-void linear_sieve(int x) {
+void linear_sieve(int x) { // O(n)
     for (int i = 2; i <= x; ++i) {
         if (leastPrime[i] == 0) {
             leastPrime[i] = i;
             Primes[pnx++] = i;
         }
-        for (int j = 0; j < pnx && Primes[j] < leastPrime[i] && i * Primes[j] <= x; ++j) {
+        for (int j = 0; j < pnx && Primes[j] <= leastPrime[i] && i * Primes[j] <= x; ++j) {
             leastPrime[i * Primes[j]] = Primes[j];
         }
     }
