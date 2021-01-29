044ca777e16a86a48cbb5e9ba8557789f9ba8004
Abd El-Twab M. fakhry <Abdeltwab.m.fakhry@gmail.com>
2021-01-29 03:02:31 +0200

    Add Linear Sieve

---
+++ 3.Mathematics/3.2.Number-Theory/3.2.1.Prime-Generation/3.2.1.5.Linear-Sieve.cpp
@@ -0,0 +1,14 @@
+int leastPrime[N];
+int Primes[664580], pnx;
+
+void linear_sieve(int x) {
+    for (int i = 2; i <= x; ++i) {
+        if (leastPrime[i] == 0) {
+            leastPrime[i] = i;
+            Primes[pnx++] = i;
+        }
+        for (int j = 0; j < pnx && Primes[j] < leastPrime[i] && i * Primes[j] <= x; ++j) {
+            leastPrime[i * Primes[j]] = Primes[j];
+        }
+    }
+}
