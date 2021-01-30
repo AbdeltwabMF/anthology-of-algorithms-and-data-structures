701392e5809690206392d29fc3deb1e6dd19e8bc
Abd El-Twab M. fakhry <Abdeltwab.m.fakhry@gmail.com>
2021-01-31 01:31:19 +0200

    Add Complexity Analysis

--- 3.Mathematics/3.2.Number-Theory/3.2.1.Prime-Generation/3.2.1.2.Segmented-Sieve.cpp
+++ 3.Mathematics/3.2.Number-Theory/3.2.1.Prime-Generation/3.2.1.2.Segmented-Sieve.cpp
@@ -1,13 +1,19 @@
 /**
 	constraints:
-	
 	1 <= l, r <= 1e{14}
 	1 <= r - l + 1 <= 1e7
-	Time complexity of this approach is : O((r − l + 1)lnln(r) + √r lnln r^{1/4})
+	2 <= x <= 1e7
+	
+	Time complexity:
+	segmented_sieve takes O((r − l + 1) * ln(ln(r)))
+	sieve takes O( + √r * ln(ln(r^{1/4})))
+	
+	Space Complexity:
+	O(MaxN + n / (ln(n) - 1.08))
 **/
 
 bool isPrime[N];
-vector<int> Primes;
+int Primes[664580], pnx; /** size of Primes = n / (ln(n) - 1.08) */
 
 void sieve(int x) {
     int basis[3] = {2, 3, 5};
@@ -18,19 +24,13 @@ void sieve(int x) {
     memset(inx, 0, sizeof(inx));
     memset(isPrime, true, sizeof(isPrime));
 
-    for (int prime : basis) {
-        if (x > prime)
-            Primes.emplace_back(prime);
-    }
-
-    for (int i = 0; i < 8; ++i) {
-        inx[wheel[i]] = i;
-    }
+    for (int p : basis) if (x > p) Primes[pnx++] = p;
+    for (int i = 0; i < 8; ++i) inx[wheel[i]] = i;
 
     int c = 0;
     for (int i = 7; i <= x; i += inc[c++]) {
         if (isPrime[i]) {
-            Primes.emplace_back(i);
+            Primes[pnx++] = i;
             int d = inx[i % 30];
             for (ll j = i * 1ll * i; j <= x; j += i * inc[d++]) {
                 isPrime[j] = false;
@@ -47,8 +47,8 @@ vector<ll> segmented_sieve(ll l, ll r) {
     vector<ll> ret;
     memset(isPrime, true, sizeof(isPrime));
 
-    for (int prime : Primes) {
-        for (ll j = max(prime * 1ll * prime, (l + prime - 1) * 1ll / prime * prime); j <= r; j += prime)
+    for (int p : Primes) {
+        for (ll j = max(p * 1ll * p, (l + p - 1) * 1ll / p * p); j <= r; j += p)
             isPrime[j - l] = false;
     }
 
