83aefc8c180bc8030f5241ec4de196ffaf696edf
Abd El-Twab M. fakhry <Abdeltwab.m.fakhry@gmail.com>
2021-01-31 01:31:13 +0200

    Add Complexity Analysis

--- 3.Mathematics/3.2.Number-Theory/3.2.1.Prime-Generation/3.2.1.1.Simple-Sieve-Eratosthenes.cpp
+++ 3.Mathematics/3.2.Number-Theory/3.2.1.Prime-Generation/3.2.1.1.Simple-Sieve-Eratosthenes.cpp
@@ -1,12 +1,16 @@
 /**
 	constraints:
-	
 	1 <= n <= 1e8
-	Time complexity of this approach is : n lnln √n + O(n)
+	
+	Time complexity:
+	O(n * ln(ln(√n)) + n)
+	
+	Space Complexity:
+	O(MaxN +  n / (ln(n) - 1.08))
 **/
 
 bool isPrime[N];
-vector<int> Primes;
+int Primes[664580], pnx; /** size of Primes = n / (ln(n) - 1.08) */
 
 void sieve(int x) {
     int basis[3] = {2, 3, 5};
@@ -17,19 +21,13 @@ void sieve(int x) {
     memset(inx, 0, sizeof(inx));
     memset(isPrime, true, sizeof(isPrime));
 
-    for (int prime : basis) {
-        if (x > prime)
-            Primes.emplace_back(prime);
-    }
-
-    for (int i = 0; i < 8; ++i) {
-        inx[wheel[i]] = i;
-    }
+    for (int p : basis) if (x > p) Primes[pnx++] = p;
+    for (int i = 0; i < 8; ++i) inx[wheel[i]] = i;
 
     int c = 0;
     for (int i = 7; i <= x; i += inc[c++]) {
         if (isPrime[i]) {
-            Primes.emplace_back(i);
+            Primes[pnx++] = i;
             int d = inx[i % 30];
             for (ll j = i * 1ll * i; j <= x; j += i * inc[d++]) {
                 isPrime[j] = false;
