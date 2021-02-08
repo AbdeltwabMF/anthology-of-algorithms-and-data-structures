f92ef7d5be4e1b1f1d011b2306ab2ab5268d492f
Abd El-Twab M. fakhry <Abdeltwab.m.fakhry@gmail.com>
2021-02-09 00:45:40 +0200

    Add limit as a param

--- 3.Mathematics/3.2.Number-Theory/3.2.4.Modified-Sieve/3.2.4.2.Mobius.cpp
+++ 3.Mathematics/3.2.Number-Theory/3.2.4.Modified-Sieve/3.2.4.2.Mobius.cpp
@@ -11,13 +11,13 @@
 
 char mob[N];
 
-void mobius_sieve() {
+void mobius_sieve(int n) {
     memset(mob, 0x02, sizeof(mob)); // 00000010
     mob[1] = 1;
-    for (int i = 2; i < N; ++i) {
+    for (int i = 2; i <= n; ++i) {
         if (mob[i] == 2) {
             mob[i] = -1;
-            for (int j = i + i; j < N; j += i) {
+            for (int j = i + i; j <= n; j += i) {
                 mob[j] = (mob[j] == 2) ? 1 : mob[j];
                 mob[j] = (j % (i * i) == 0) ? 0 : -mob[j];
             }
